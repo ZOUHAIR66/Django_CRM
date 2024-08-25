8958675fd0280fd07ecd7175844b483aacd49767
Zouhair Mestari <mestari8225@gmail.com>
2024-08-25 14:24:48 +0200

    Final Project

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     #path('login/',views.login_user,name='login'),
     path('logout/',views.logout_user,name='logout'),
     path('register/',views.register_user,name='register'),
+    path('record/<int:pk>',views.customer_record,name='record'),
+    path('delete/<int:pk>',views.delete_user,name='delete'),
+    path('update/<int:pk>',views.update_record,name='update'),
+    path('add/',views.add_record,name='add'),
 ]
